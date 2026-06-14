43fff1571e4f0fa97dc4588a8ddb7b01a64d82d6
AlexOrekhov <apexof@gmail.com>
2026-06-14 13:42:13 +0300

    refactor: configure QueryClient with default options for queries

--- frontend/src/app/providers.tsx
+++ frontend/src/app/providers.tsx
@@ -1,7 +1,14 @@
 import { type FC, type PropsWithChildren } from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 1,
+      staleTime: 30_000,
+    },
+  },
+});
 
 export const Providers: FC<PropsWithChildren> = (props) => {
   const { children } = props;
